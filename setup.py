a0bd144a22d81d00d4df4a6ecdc45b91c412bf53
arshadshk <ars.arshad.ars@gmail.com>
2023-01-13 20:13:12 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+import setuptools
+
+setup(name='Position Prediction Pretraining',
+      version='1.0.0',
+      description='',
+      author='Arshad Shaikh',
+      author_email='arshaikh5775@gmail.com',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
